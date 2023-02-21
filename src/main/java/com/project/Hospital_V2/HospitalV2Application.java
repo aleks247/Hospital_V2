f404efa4127c15649e3f8e1eb36f79e7de8c372d
Aleks247 <aleks247@abv.bg>
2023-02-21 21:22:01 +0200

    Creating project

---
+++ src/main/java/com/project/Hospital_V2/HospitalV2Application.java
@@ -0,0 +1,13 @@
+package com.project.Hospital_V2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Hospital_V2/HospitalV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Hospital_V2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
