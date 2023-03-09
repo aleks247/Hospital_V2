54be4e99beb4082a2fecd6eae43e30fe6527639a
Aleks247 <aleks247@abv.bg>
2023-03-09 17:57:08 +0200

    Making details

--- src/main/java/com/project/Hospital_V2/controllers/DoctorController.java
+++ src/main/java/com/project/Hospital_V2/controllers/DoctorController.java
@@ -1,14 +1,28 @@
 package com.project.Hospital_V2.controllers;
 
+import com.project.Hospital_V2.entities.Appointment;
+import com.project.Hospital_V2.repositories.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/doctors")
 public class DoctorController {
+    @Autowired
+    AppointmentRepository appointmentRepository;
+
     @GetMapping("/menu")
-    public String main(){
+    public String menu(){
         return "doctors/menu";
+    }
+
+    @GetMapping
+    public String getAllResorts(Model model) {
+        Iterable<Appointment> appointments = appointmentRepository.findAll();
+        model.addAttribute("appointments", appointments);
+        return "/doctors/menu";
     }
 }
--- src/main/java/com/project/Hospital_V2/controllers/PatientController.java
+++ src/main/java/com/project/Hospital_V2/controllers/PatientController.java
@@ -1,8 +1,19 @@
 package com.project.Hospital_V2.controllers;
 
+import com.project.Hospital_V2.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/patients")
 public class PatientController {
+    @Autowired
+    PatientRepository patientRepository;
 
+    @GetMapping("/menu")
+    public String menu(){
+        return "patients/menu";
+    }
 }
