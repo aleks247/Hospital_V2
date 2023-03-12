472934a8800be0749d3dc096aa56b1677fc57696
Aleks247 <aleks247@abv.bg>
2023-03-12 19:24:56 +0200

    Making validation on appointment creation

--- src/main/java/com/project/Hospital_V2/repositories/AppointmentRepository.java
+++ src/main/java/com/project/Hospital_V2/repositories/AppointmentRepository.java
@@ -1,9 +1,13 @@
 package com.project.Hospital_V2.repositories;
 
 import com.project.Hospital_V2.entities.Appointment;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AppointmentRepository extends CrudRepository<Appointment, Integer> {
+    List<Appointment> findByDoctorId(Integer DoctorId);
 }
