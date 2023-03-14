4b8513a2c338dbd735f1a283fbdadc3acf751cf4
Asus <an.lyubenov@abv.bg>
2023-03-14 21:34:28 +0200

    edit PatientService and PatientController

--- src/main/java/com/project/Hospital_V2/controllers/PatientController.java
+++ src/main/java/com/project/Hospital_V2/controllers/PatientController.java
@@ -1,26 +1,17 @@
 package com.project.Hospital_V2.controllers;
-
 import com.project.Hospital_V2.entities.Appointment;
-import com.project.Hospital_V2.entities.Doctor;
-import com.project.Hospital_V2.entities.Patient;
-import com.project.Hospital_V2.enums.KindOfReview;
 import com.project.Hospital_V2.repositories.AppointmentRepository;
 import com.project.Hospital_V2.repositories.DoctorRepository;
 import com.project.Hospital_V2.repositories.PatientRepository;
 import com.project.Hospital_V2.services.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+
 
 @Controller
 @RequestMapping("/patients")
@@ -35,27 +26,12 @@ public class PatientController {
     private PatientService patientService;
 
     @GetMapping("/menu")
-    public String menu(Model model){
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String name = auth.getName();
-        Patient patient = patientRepository.findByUsername(name);
-        Iterable<Appointment> appointmentIterable = appointmentRepository.findAll();
-        List<Appointment> appointments = new ArrayList<>();
-        for (Appointment appointment: appointmentIterable) {
-            if(appointment.getPatient().equals(patient)){
-                appointments.add(appointment);
-            }
-        }
-        model.addAttribute("appointments", appointments);
+    public String menu(Model model) {
         return "patients/menu";
     }
 
     @GetMapping("/create")
     public String appointmentForm(Model model) {
-        Iterable<Doctor> doctors = doctorRepository.findAll();
-        model.addAttribute("appointment", new Appointment());
-        model.addAttribute("doctors", doctors);
-        model.addAttribute("KindOfReview", KindOfReview.values());
         return "/patients/create";
     }
 
@@ -68,30 +44,17 @@ public class PatientController {
 
     @GetMapping("/edit/{appointmentId}")
     public String editAppointment(@PathVariable(name = "appointmentId") Integer appointmentId, Model model) {
-        Optional<Appointment> optionalAppointment = appointmentRepository.findById(appointmentId);
-        if (optionalAppointment.isPresent()) {
-            model.addAttribute("appointment", optionalAppointment.get());
-        } else {
-            model.addAttribute("appointment", "Error!");
-            model.addAttribute("errorMsg", " Not existing appointment with id = " + appointmentId);
-        }
-
         return "/patients/edit";
     }
 
     @PostMapping("/update")
     public ModelAndView updateAppointment(@Valid Appointment appointment, BindingResult bindingResult) {
-        if(bindingResult.hasErrors()){
-            return new ModelAndView("/patients/edit");
-        } else {
-            appointmentRepository.save(appointment);
-            return new ModelAndView("redirect:/patients/menu");
-        }
+        return new ModelAndView("redirect:/patients/menu");
     }
+
 
     @PostMapping("/delete/{appointmentId}")
     public ModelAndView deleteAppointment(@PathVariable(name = "appointmentId") Integer appointmentId) {
-        appointmentRepository.deleteById(appointmentId);
         return new ModelAndView("redirect:/patients/menu");
     }
 }
--- src/main/java/com/project/Hospital_V2/services/PatientService.java
+++ src/main/java/com/project/Hospital_V2/services/PatientService.java
@@ -1,18 +1,25 @@
 package com.project.Hospital_V2.services;
-
 import com.project.Hospital_V2.entities.Appointment;
+import com.project.Hospital_V2.entities.Doctor;
 import com.project.Hospital_V2.entities.Patient;
+import com.project.Hospital_V2.enums.KindOfReview;
 import com.project.Hospital_V2.repositories.AppointmentRepository;
+import com.project.Hospital_V2.repositories.DoctorRepository;
 import com.project.Hospital_V2.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
-
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PatientService {
@@ -21,6 +28,10 @@ public class PatientService {
     private AppointmentRepository appointmentRepository;
     @Autowired
     private PatientRepository patientRepository;
+    @Autowired
+    DoctorRepository doctorRepository;
+    @Autowired
+    private PatientService patientService;
 
     public PatientService(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -33,9 +44,9 @@ public class PatientService {
         int rowsAffected = jdbcTemplate.update(sql, params);
     }
 
-    public ModelAndView appointmentCreation(Appointment appointment,BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
-            return  new ModelAndView("redirect:/patients/create");
+    public ModelAndView appointmentCreation(Appointment appointment, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ModelAndView("redirect:/patients/create");
         } else {
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
             String name = auth.getName();
@@ -64,4 +75,62 @@ public class PatientService {
             }
         }
     }
+
+    public String menu(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        Patient patient = patientRepository.findByUsername(name);
+        Iterable<Appointment> appointmentIterable = appointmentRepository.findAll();
+        List<Appointment> appointments = new ArrayList<>();
+        for (Appointment appointment : appointmentIterable) {
+            if (appointment.getPatient().equals(patient)) {
+                appointments.add(appointment);
+            }
+        }
+        model.addAttribute("appointments", appointments);
+        return "patients/menu";
+    }
+
+
+    public String appointmentForm(Model model) {
+        Iterable<Doctor> doctors = doctorRepository.findAll();
+        model.addAttribute("appointment", new Appointment());
+        model.addAttribute("doctors", doctors);
+        model.addAttribute("KindOfReview", KindOfReview.values());
+        return "/patients/create";
+    }
+
+
+    public ModelAndView appointmentSubmit(@ModelAttribute("appointment") @Valid Appointment appointment, BindingResult bindingResult) {
+        return patientService.appointmentCreation(appointment, bindingResult);
+    }
+
+
+    public String editAppointment(@PathVariable(name = "appointmentId") Integer appointmentId, Model model) {
+        Optional<Appointment> optionalAppointment = appointmentRepository.findById(appointmentId);
+        if (optionalAppointment.isPresent()) {
+            model.addAttribute("appointment", optionalAppointment.get());
+        } else {
+            model.addAttribute("appointment", "Error!");
+            model.addAttribute("errorMsg", " Not existing appointment with id = " + appointmentId);
+        }
+
+        return "/patients/edit";
+    }
+
+    public ModelAndView updateAppointment(@Valid Appointment appointment, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ModelAndView("/patients/edit");
+        } else {
+            appointmentRepository.save(appointment);
+            return new ModelAndView("redirect:/patients/menu");
+        }
+    }
+
+
+    public ModelAndView deleteAppointment(@PathVariable(name = "appointmentId") Integer appointmentId) {
+        appointmentRepository.deleteById(appointmentId);
+        return new ModelAndView("redirect:/patients/menu");
+    }
 }
+
